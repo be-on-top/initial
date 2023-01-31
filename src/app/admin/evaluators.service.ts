d0de08ec4f45e8ad05db52426c8c2052afb0ed70
montussan2023! <pwacitypro@gmail.com>
2023-01-31 14:58:28 +0100

    Deleting user from fireAuth on delete of related doc from evaluators collection

--- src/app/admin/evaluators.service.ts
+++ src/app/admin/evaluators.service.ts
@@ -2,8 +2,8 @@ import { Injectable } from '@angular/core';
 import { Evaluators } from './evaluators';
 
 // à vérifier
-import { Auth,createUserWithEmailAndPassword, user } from '@angular/fire/auth';
-import { Firestore, collectionData, collection, documentId, getDoc, docData, updateDoc, setDoc } from '@angular/fire/firestore';
+import { Auth, createUserWithEmailAndPassword} from '@angular/fire/auth';
+import { Firestore, collectionData, collection, documentId, getDoc, docData, setDoc } from '@angular/fire/firestore';
 import { addDoc, deleteDoc, doc, getDocs } from 'firebase/firestore';
 import { Observable } from 'rxjs';
 
@@ -16,56 +16,59 @@ export class EvaluatorsService {
 
   // évaluateurs ne serait pas un tableau de type any mais un observable
   evaluators: any[] = [];
-  result:any;
+  result: any;
 
 
-  constructor(private auth: Auth, private firestore:Firestore) { }
+  constructor(private auth: Auth, private firestore: Firestore) { }
 
   async createEvaluator(evaluator: any) {
 
     // let newEvaluator = { id: Date.now(), ...evaluator };
-    let newEvaluator = { created: Date.now(), roles:'admin-', ...evaluator };
+    let newEvaluator = { created: Date.now(), roles: 'admin-', ...evaluator };
     this.evaluators = [newEvaluator, ...this.evaluators];
     console.log(this.evaluators);
     // on va lui affecter un password aléatoire en fonction de la date
-    let password= Math.random().toString(36).slice(2) + Math.random().toString(36).toUpperCase().slice(2);
-    
-    // enregistrement en base dans fireAuth d'une part : 
-   this.result= await createUserWithEmailAndPassword(this.auth, evaluator.email, password);  
+    let password = Math.random().toString(36).slice(2) + Math.random().toString(36).toUpperCase().slice(2);
 
-   if (this.result && this.result.user) {
-    const {uid, emailVerified}=this.result.user;
-    newEvaluator.id=this.result.user.uid
-    }   
-    
+    // enregistrement en base dans fireAuth d'une part : 
+    this.result = await createUserWithEmailAndPassword(this.auth, evaluator.email, password);
+
+    if (this.result && this.result.user) {
+      const { uid, emailVerified } = this.result.user;
+      newEvaluator.id = this.result.user.uid
+    }
+
     // enregistre dans Firestore d'autre part avec un collection evaluators qui elle aura de multiples propriétés
     let $evaluatorsRef = collection(this.firestore, "evaluators");
-    addDoc($evaluatorsRef, newEvaluator)    
+    addDoc($evaluatorsRef, newEvaluator)
   }
 
   // getEvaluators(): Observable<Evaluators[]> {
-  getEvaluators(){
+  getEvaluators() {
     let $evaluatorsRef = collection(this.firestore, "evaluators");
-    return collectionData($evaluatorsRef,{idField:"id"}) as Observable<Evaluators[]>
+    return collectionData($evaluatorsRef, { idField: "id" }) as Observable<Evaluators[]>
 
   }
 
   deleteEvaluator(id: string) {
-    // on utilisera la méthode deleteDoc() de Firestore
-    let $evaluatorRef = doc(this.firestore, "evaluators/"+id)
+    // on utilisera la méthode deleteDoc() de Firestore et delete de currentUser
+    let $evaluatorRef = doc(this.firestore, "evaluators/" + id)
+    console.log("this.auth.currentUser", this.auth.currentUser);
+    this.auth.currentUser?.delete()
     return deleteDoc($evaluatorRef);
   }
 
+
   getEvaluator(id: string) {
     // on utilisera la méthode deleteDoc() de Firestore
-    let $evaluatorRef = doc(this.firestore, "evaluators/"+id)
-    return docData($evaluatorRef, {idField:'id'}) as Observable<Evaluators>;
+    let $evaluatorRef = doc(this.firestore, "evaluators/" + id)
+    return docData($evaluatorRef, { idField: 'id' }) as Observable<Evaluators>;
   }
 
 
-  updateEvaluator(id: string, evaluator:Evaluators) {    
-    let $evaluatorRef = doc(this.firestore, "evaluators/"+id);
-    setDoc($evaluatorRef,evaluator)  
+  updateEvaluator(id: string, evaluator: Evaluators) {
+    let $evaluatorRef = doc(this.firestore, "evaluators/" + id);
+    setDoc($evaluatorRef, evaluator)
   }
 
 
