1fea37dbb915656c6a17c39432ed49517361ccf3
montussan2023! <pwacitypro@gmail.com>
2023-03-20 09:27:33 +0100

    fixing and optimisation questionsService for getting and displaying mediasResponses & mediaQuestion with const

--- src/app/admin/questions.service.ts
+++ src/app/admin/questions.service.ts
@@ -12,8 +12,8 @@ import { Observable } from 'rxjs';
 export class QuestionsService {
 
   image: any;
-  mediaQuestions: string[]
-  mediaResponses: string[]
+  // mediaQuestions: string[]
+  // mediaResponses: string[]
   pathResponses: string[]
 
   questions: any[] = [];
@@ -28,13 +28,12 @@ export class QuestionsService {
   idMediaOption4: string = ""
 
   registryNumbers: any = []
-  mediasResponsesById: any = []
-  // on part ici de l'hypothèse où on en a plusieurs
-  mediaQuestionById: any = []
+  // mediasResponsesById: any = []
+  // mediaQuestionById: any = []
 
   constructor(private storage: Storage, private firestore: Firestore) {
-    this.mediaQuestions = []
-    this.mediaResponses = []
+    // this.mediaQuestions = []
+    // this.mediaResponses = []
     this.pathResponses = []
   }
 
@@ -119,7 +118,30 @@ export class QuestionsService {
     }
   }
 
+  
+  // getMediasQuestions(): any {
+  //   // Create a reference under which you want to list 
+  //   let mediasQuestionsRef = ref(this.storage, 'images/questions');
+  //   // Find all the prefixes and items.
+  //   listAll(mediasQuestionsRef)
+  //     .then(async response => {
+  //       console.log("listAll response for mediasQuestions", response);
+  //       for (let item of response.items) {
+  //         let url = await getDownloadURL(item);
+  //         console.log("url renvoyée en boucle", url);
+  //         this.mediaQuestions.push(url)
+  //       }
+
+  //     }).catch((error) => {
+  //       // Uh-oh, an error occurred!
+  //     });
+
+  //   return this.mediaQuestions
+
+  // }
+  // refacto avec const pour éviter les doublons !!!!! 
   getMediasQuestions(): any {
+    const mediaQuestions: string[] = []
     // Create a reference under which you want to list 
     let mediasQuestionsRef = ref(this.storage, 'images/questions');
     // Find all the prefixes and items.
@@ -129,18 +151,45 @@ export class QuestionsService {
         for (let item of response.items) {
           let url = await getDownloadURL(item);
           console.log("url renvoyée en boucle", url);
-          this.mediaQuestions.push(url)
+          mediaQuestions.push(url)
         }
 
       }).catch((error) => {
         // Uh-oh, an error occurred!
       });
 
-    return this.mediaQuestions
+    return mediaQuestions
 
   }
 
+  // getMediasResponses(): any {
+  //   // Create a reference under which you want to list 
+  //   let mediasResponsesRef = ref(this.storage, 'images/questions/responses');
+  //   // Find all the prefixes and items.
+  //   listAll(mediasResponsesRef)
+  //     .then(async response => {
+  //       console.log("listAll response for mediasQuestions", response);
+  //       for (let item of response.items) {
+  //         console.log("voila la réponse avec getDownloaURL !!!!!!!!!!!!!!!!!!!!!!!!!!!", item);
+
+  //         // est-ce que c'était pertinent d'avoir l'url plutôt que le chemin ?
+  //         let url = await getDownloadURL(item);
+  //         console.log("url renvoyée en boucle", url);
+  //         this.mediaResponses.push(url)
+  //         // this.pathResponses.push(item.fullPath)
+  //       }
+
+  //     }).catch((error) => {
+  //       // Uh-oh, an error occurred!
+  //     });
+
+  //   return (this.mediaResponses)
+
+  // }
+
+  // refacto avec const pour éviter les doublons
   getMediasResponses(): any {
+    const mediaResponses:string[] = []
     // Create a reference under which you want to list 
     let mediasResponsesRef = ref(this.storage, 'images/questions/responses');
     // Find all the prefixes and items.
@@ -153,7 +202,7 @@ export class QuestionsService {
           // est-ce que c'était pertinent d'avoir l'url plutôt que le chemin ?
           let url = await getDownloadURL(item);
           console.log("url renvoyée en boucle", url);
-          this.mediaResponses.push(url)
+          mediaResponses.push(url)
           // this.pathResponses.push(item.fullPath)
         }
 
@@ -161,7 +210,7 @@ export class QuestionsService {
         // Uh-oh, an error occurred!
       });
 
-    return (this.mediaResponses)
+    return (mediaResponses)
 
   }
 
@@ -204,14 +253,13 @@ export class QuestionsService {
 
     // un peu comme pour la création
     console.log("question ou les valeurs du formulaire", question);
-
     console.log('object.values allFilesToUpdate', Object.values(allFilesToUpdate));
 
 
     if (question.mediaQuestion) {
       this.idMediaQuestion = `mediaQuestionN${question.number}_${question.sigle}`
       // c'est là qu'on peut intégrer une différence selon le type de fichier détecté
-      console.log("dddddddddddddddddddddddddddddddddddddddddddddddddddddddddddd", Object.values(allFilesToUpdate)[0]);
+      console.log("allFilesToUpdate depuis questionsService", Object.values(allFilesToUpdate)[0]);
 
       if (Object.values(allFilesToUpdate)[0] == "video/mp4") {
         this.isVideo = true;
@@ -276,46 +324,80 @@ export class QuestionsService {
   }
 
 
+  // pour éviter affichage en doublon, attention à déclarer mediaResponsesById comme constante !!!! 
+  // getMediasResponsesById(id: string) {
+  //   let mediasResponsesRef = ref(this.storage, 'images/questions/responses');
+  //   listAll(mediasResponsesRef)
+  //     .then(async response => {
+  //       for (let item of response.items) {
+  //         console.log("esssai recuperation media by id", item.fullPath.includes(id));
+  //         item.fullPath.includes(id) == true ? this.mediasResponsesById.push(item.fullPath.includes(id)) : ""
+  //         console.log("mediaREspnsesById", this.mediasResponsesById);
+  //       }
+
+  //     }).catch((error) => {
+  //       // Uh-oh, an error occurred!
+  //     });
+
+  //   return (this.mediasResponsesById)
+
+  // }
+
   getMediasResponsesById(id: string) {
-    // Create a reference under which you want to list 
+    const mediasResponsesById:any=[]
     let mediasResponsesRef = ref(this.storage, 'images/questions/responses');
-    // Find all the prefixes and items.
     listAll(mediasResponsesRef)
       .then(async response => {
-        // console.log("listAll path response for mediasResponses", response);
         for (let item of response.items) {
-
           console.log("esssai recuperation media by id", item.fullPath.includes(id));
-          item.fullPath.includes(id) == true ? this.mediasResponsesById.push(item.fullPath.includes(id)) : ""
-          console.log("mediaREspnsesById", this.mediasResponsesById);
+          item.fullPath.includes(id) == true ? mediasResponsesById.push(item.fullPath.includes(id)) : ""
+          // console.log("mediaREspnsesById", mediasResponsesById);
         }
 
       }).catch((error) => {
         // Uh-oh, an error occurred!
       });
 
-    return (this.mediasResponsesById)
+    return (mediasResponsesById)
 
   }
 
+  // getMediaQuestionById(id: string) {
+  //   let mediasQuestionsRef = ref(this.storage, 'images/questions');
+  //   listAll(mediasQuestionsRef)
+  //     .then(async response => {
+  //       // console.log("listAll medias for mediasQuestions", response);
+  //       for (let item of response.items) {
+  //         // console.log("esssai recuperation media question by id", item.fullPath.includes(id));
+  //         item.fullPath.includes(id) == true ? this.mediaQuestionById.push(item) : ""
+  //         console.log("mon media Question pour l'id", this.mediaQuestionById);
+  //       }
+
+  //     }).catch((error) => {
+  //       // Uh-oh, an error occurred!
+  //     });
+
+  //   return (this.mediaQuestionById)
+
+  // }  
+
   getMediaQuestionById(id: string) {
-    // Create a reference under which you want to list 
+    const mediaQuestionById: any = []
     let mediasQuestionsRef = ref(this.storage, 'images/questions');
-    // Find all the prefixes and items.
     listAll(mediasQuestionsRef)
       .then(async response => {
         // console.log("listAll medias for mediasQuestions", response);
         for (let item of response.items) {
           // console.log("esssai recuperation media question by id", item.fullPath.includes(id));
-          item.fullPath.includes(id) == true ? this.mediaQuestionById.push(item) : ""
-          console.log("mon media Question pour l'id", this.mediaQuestionById);
+          item.fullPath.includes(id) == true ? mediaQuestionById.push(item) : ""
+          // console.log("mon media Question pour l'id", mediaQuestionById);
         }
 
       }).catch((error) => {
         // Uh-oh, an error occurred!
       });
 
-    return (this.mediaQuestionById)
+    return (mediaQuestionById)
 
   }  
 
