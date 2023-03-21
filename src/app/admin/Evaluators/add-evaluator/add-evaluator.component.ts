0905826a8808aa1f1aba145affdb1e5196d91bc5
montussan2023! <pwacitypro@gmail.com>
2023-03-21 15:07:49 +0100

    New sigle properties on evaluators account with select multiple added on evaluators collection

--- src/app/admin/Evaluators/add-evaluator/add-evaluator.component.ts
+++ src/app/admin/Evaluators/add-evaluator/add-evaluator.component.ts
@@ -12,6 +12,7 @@ export class AddEvaluatorComponent implements OnInit {
   lastName: string = "active";
   firstName: string = "";
   email: string = "";
+  selectedSigles: string[] = []
 
   constructor(private service: EvaluatorsService, private router: Router) { }
 
@@ -36,20 +37,55 @@ export class AddEvaluatorComponent implements OnInit {
 
   }
 
+  // pour affecation métier de l'évaluateur
+  checkIfSelected(sigle: any) {
+    console.log(sigle);
+    this.selectedSigles = [...this.selectedSigles, sigle]
+  }
+
   // juste pour l'exercice à virer !!! 
-  checkLength(field:any){
+  checkLength(field: any) {
     console.log("eeeeeee", field.value);
-    field.value.length<8? alert(`la longueur de ${field.value} est de ${field.value.length} alors qu'un minimum de 8 est requis`):""
+    field.value.length < 8 ? console.log(`la longueur de ${field.value} est de ${field.value.length} alors qu'un minimum de 8 est requis`) : ""
     // alert(`la longueur de ${e.value} est ${e.value.length}`)    
   }
 
   // juste pour l'exercice à virer !!!
-  change(event:any){
+  change(event: any) {
     console.log("event target value", event.target.value);
-    event.target.value.length>6?alert("Ok"):""
+    event.target.value.length > 6 ? alert("Ok") : ""
 
   }
 
+  /** La fonction qui écoute l'événement onclick sur le  password juste pour l'exercice */
+  toggleDisplayPassword(event: any) {
 
+    /* La source de l'événement */
+    const srcElement = event.srcElement;
+
+    /* Récupère l'élément enfant qui précède l'élément source */
+    const passwordField = srcElement.previousElementSibling;
+
+    /* Si le champ est de type "mot de passe" */
+    if (passwordField.type == "password") {
+
+      /* Interversion des icônes */
+      srcElement.classList.remove("bi-eye");
+      srcElement.classList.add("bi-eye-slash");
+
+      /* Affiche le texte en clair */
+      passwordField.type = "text";
+
+    } else if (passwordField.type == "text") {
+
+      /* Interversion des icônes */
+      srcElement.classList.remove("bi-eye-slash");
+      srcElement.classList.add("bi-eye");
+
+      /* Cache le texte du mot de passe */
+      passwordField.type = "password";
+    }
+
+  }
 
 }
