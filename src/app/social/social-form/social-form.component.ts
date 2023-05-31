befe179a66ed36c95bef347833da24f2f6b82fa4
montussan2023! <pwacitypro@gmail.com>
2023-05-31 09:50:08 +0200

    changing onSubmit method for socialEnrollmentForm & card header titles to simplify display on mobiles

--- src/app/social/social-form/social-form.component.ts
+++ src/app/social/social-form/social-form.component.ts
@@ -1,7 +1,7 @@
 import { query } from '@angular/animations';
 import { Component, OnInit } from '@angular/core';
 import { Auth, onAuthStateChanged } from '@angular/fire/auth';
-import { Firestore, addDoc, collection, getDocs, where } from '@angular/fire/firestore';
+import { Firestore, addDoc, collection, doc, getDocs, setDoc, where } from '@angular/fire/firestore';
 import { NgForm } from '@angular/forms';
 import { Router } from '@angular/router';
 import { StudentsService } from 'src/app/admin/students.service';
@@ -47,47 +47,29 @@ export class SocialFormComponent implements OnInit {
     })
 
 
-    // onAuthStateChanged(this.auth, (user: any) => {
-    //   if (user) {
-    //     this.authId = user.uid
-    //     this.service.getStudentById(user.uid).subscribe();
-    //     console.log("UID: ", user.uid);
-    //     console.log("Email: ", user.email);
-    //     this.getDocsByParam(this.authId)
-    //   }
-    //   else {
-    //     console.log("Personne n'est authentifié actuellement !");
-    //   }
-    // })
+    onAuthStateChanged(this.auth, (user: any) => {
+      if (user) {
+        this.uid = user.uid
+      }
+      else {
+        console.log("Personne n'est authentifié actuellement !");
+      }
+    })
   }
-  // async getDocsByParam(uid: string) {
-  //   const myData = query(collection(this.firestore, 'students'), where('id', '==', uid));
-  //   const querySnapshot = await getDocs(myData);
-  //   querySnapshot.forEach((doc) => {
-  //     console.log(doc.id, ' => ', doc.data());
-  //     this.userData = doc.data()
-  //     // Assignez les valeurs récupérées aux propriétés correspondantes
-  //     this.firstName = this.userData.firstName;
-  //     this.lastName = this.userData.lastName;
-  //     this.email = this.userData.email;
-  //     console.log("this.userData", this.userData);
-  //   });
-  // }
+
 
   async onSubmit(form: NgForm) {
     console.log("form value", form.value);
 
-    const socialFormData = form.value; // Copie des valeurs du formulaire
+    // const socialFormData = form.value; 
 
     try {
-      // Ajoutez le nouvel objet à la collection "SocialForm" dans Firestore
-      // const docRef = await addDoc(collection(this.firestore, 'SocialForm'), socialFormData);
-      // console.log('Document ID: ', docRef.id);
-
-      // form.reset();
-      // this.router.navigate(['/']);
+      let enrollRef = collection(this.firestore, "SocialForm")
+      setDoc(doc(enrollRef, this.uid), form.value)
+      form.reset()
+      this.router.navigate(['/'])
     } catch (error) {
-      console.error('Erreur lors de l\'enregistrement des données: ', error);
+      console.error('Erreur lors de l\'enregistrement des données: ', error)
     }
   }
 
