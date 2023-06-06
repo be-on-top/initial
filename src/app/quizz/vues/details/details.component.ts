1940a517d613529570028069832123a1e8ae9654
montussan2023! <pwacitypro@gmail.com>
2023-06-06 11:04:17 +0200

    setting counters fullGoodAnswersClicked fullOptScoringTrue fullAnswersClicked totalAnswersAvailable for new rules on optScorings

--- src/app/quizz/vues/details/details.component.ts
+++ src/app/quizz/vues/details/details.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { Component, Input, Output, EventEmitter, OnInit } from '@angular/core';
 // import { Firestore, collection, orderBy, startAt, startAfter, query, where, limit } from '@angular/fire/firestore';
 // import { QuestionsService } from 'src/app/admin/questions.service';
 
@@ -7,13 +7,22 @@ import { Component, Input, Output, EventEmitter } from '@angular/core';
   templateUrl: './details.component.html',
   styleUrls: ['./details.component.css']
 })
-export class DetailsComponent {
+export class DetailsComponent implements OnInit {
 
   counter: number = 0
   // faut qu'il provienne du  parent... incrémenté depuis le parent
   // counterQuestionNumber: number=0
 
-  @Input() q: any;
+  // puisqu'on a décidé de ne comptabiliser les bonnes réponses qu'à la condition qu'elles soient TOUTES bonnes, 
+  // il va falloir au fur et à mesure qu'une bonne réponse est cliquée, l'ajouter à un compteur de bonnes réponses. 
+  // Si au final, la valeur de ce compteur de bonnes réponses cliquées est équivalent à la valeur du compteur des bonnes réponses en base, alors on raffle la mise
+  fullGoodAnswersClicked: number = 0
+  fullOptScoringTrue: number = 0
+  fullAnswersClicked: number = 0
+  totalAnswersAvailable: number = 0
+
+
+  @Input() q: any
   @Input() questionsMedias: any
   @Input() responsesMedias: any
   // pour prévenir le parent qu'au minimum un clic a été détecté donc une réponse donnée (quelle que soit sa valeur)
@@ -24,6 +33,22 @@ export class DetailsComponent {
   }
 
   ngOnInit() {
+    console.log("this.q.optScoring1", this.q.optScoring1 === 'false');
+
+    // on initialise la valeur réelle de fullOptScoringArray pour avoir un point de comparaison
+    this.q.optScoring1 === 'true' ? this.fullOptScoringTrue = Number(this.fullOptScoringTrue) + 1 : ""
+    this.q.optScoring2 === 'true' ? this.fullOptScoringTrue = Number(this.fullOptScoringTrue) + 1 : ""
+    this.q.optScoring3 === 'true' ? this.fullOptScoringTrue = Number(this.fullOptScoringTrue) + 1 : ""
+    this.q.optScoring4 === 'true' ? this.fullOptScoringTrue = Number(this.fullOptScoringTrue) + 1 : ""
+    console.log("this.fullOptScoringArray", this.fullOptScoringTrue)
+
+    // on initialise la valeur réelle de totalAnswersAvailable pour la limite à 2, 3 ou 4 réponses max
+    this.q.optScoring1 !== '' ? this.totalAnswersAvailable = Number(this.totalAnswersAvailable) + 1 : ""
+    this.q.optScoring2 !== '' ? this.totalAnswersAvailable = Number(this.totalAnswersAvailable) + 1 : ""
+    this.q.optScoring3 !== '' ? this.totalAnswersAvailable = Number(this.totalAnswersAvailable) + 1 : ""
+    this.q.optScoring4 !== '' ? this.totalAnswersAvailable = Number(this.totalAnswersAvailable) + 1 : ""
+    console.log("this.totalAnswersAvailable", this.totalAnswersAvailable)
+
   }
 
   // ngOnDestroy(): void {
@@ -31,18 +56,28 @@ export class DetailsComponent {
   // }
 
   choice(optScoring: any) {
-    // test OK
-    // optScoring == "true" ? alert("Bingo ! ") : alert("Mauvaise réponse ! ")
-    if (optScoring == "true") {
-      this.counter =   this.counter + Number(this.q.notation) 
-      alert(`etat du compter avec les notations ${this.counter}`)
-    } else { alert('Aucun point supplémentnaire') }
-    // ici, on enregistrera sûrement en base
+    // on incrémente le nombre total de réponses cliquées (ou cochées)
+    this.fullAnswersClicked++
+    if (optScoring === 'true') {
+      // on incrémente le nombre de bonnes réponses données
+      this.fullGoodAnswersClicked++
+      console.log("this.fullGoodAnswersClicked", this.fullGoodAnswersClicked);
+      this.fullGoodAnswersClicked === this.fullOptScoringTrue ? this.counter = this.counter + Number(this.q.notation) : ""
+      // this.fullGoodAnswersClicked>this.fullOptScoringTrue?alert("Vous devez faire un choix. Toutes les réponses ne peuvent être bonnes"): ""
+      // this.fullAnswersClicked === 4 ? (alert("Vous ne pouvez pas cocher toutes les réponses. Il faut faire une sélection"), this.fullAnswersClicked = 0, this.fullGoodAnswersClicked = 0, this.counter = 0) : ""
+      // console.log("this.fullGoodAnswersClicked", this.fullAnswersClicked)
+      // comme on peut en réalité avoir un nombre de réponse entre 2, 3 et 4...
+      this.fullAnswersClicked === this.totalAnswersAvailable ? (alert("Vous ne pouvez pas cocher toutes les réponses. Il faut faire une sélection"), this.fullAnswersClicked = 0, this.fullGoodAnswersClicked = 0, this.counter = 0) : ""
+      console.log("this.fullGoodAnswersClicked", this.fullAnswersClicked)
 
-    // et on fait remonter l'information : une réponse a bien été cliquée ! 
+    }
+
+
+    // ici, on enregistrera sûrement en base !!!!
+
+    // et on fait remonter l'information : une réponse a bien été cliquée, ce qui en soit suffit pour pouvoir passer à la suivante ! 
     this.isCompleted = true
     this.hasBeenClicked.emit(this.isCompleted)
-
 
   }
 
