4ac1b5383effc4a23a71f3a426c86b0dcf915000
montussan2023! <pwacitypro@gmail.com>
2023-09-07 08:35:15 +0200

    fixing controls on detectFiles, uploadBytes and loadImage(getDownloadURL)

--- src/app/admin/settings/update-trades/cover-image/cover-image.component.ts
+++ src/app/admin/settings/update-trades/cover-image/cover-image.component.ts
@@ -20,6 +20,7 @@ export class CoverImageComponent {
   tradeId: string = ''; // Assurez-vous que cette variable est initialisée avec la valeur appropriée
   imageUrl: string = ''; // Pour stocker l'URL de l'image
   imageFile: File | null = null; // Pour stocker la nouvelle image à télécharger
+  imageSelected = false; // Pour conditionner l'affichage du message d'erreur
 
   constructor(
     private service: SettingsService,
@@ -31,7 +32,7 @@ export class CoverImageComponent {
   ngOnInit(): void {
     this.tradeId = this.ac.snapshot.params['id'];
 
-    // Chargez l'URL de l'image existante si elle existe
+    // Charge l'URL de l'image existante si elle existe
     this.loadImage();
   }
 
@@ -41,34 +42,57 @@ loadImage() {
   const imageRef = ref(this.storage, 'images/trades/' + this.tradeId + '.jpeg');
   const rootRef = ref(this.storage); // Référence racine de Firebase Storage
 
-  // Vérifiez d'abord si l'objet existe
-  listAll(rootRef)
-    .then((result) => {
-      const itemRef = result.items.find((item) => item.name === imageRef.name);
-      if (itemRef) {
-        // L'image existe, obtenez l'URL
-        getDownloadURL(itemRef)
-          .then((url) => {
-            this.imageUrl = url;
-          })
-          .catch((error) => {
-            console.error("Erreur lors du chargement de l'image", error);
-          });
-      }
-    })
-    .catch((error) => {
-      console.error("Erreur lors de la vérification de l'existence de l'objet", error);
-    });
+  // // Vérifie d'abord si l'objet existe
+  // listAll(rootRef)
+  //   .then((result) => {
+  //     const itemRef = result.items.find((item) => item.name === imageRef.name);
+  //     if (itemRef) {
+  //       // L'image existe, obtenir l'URL
+  //       getDownloadURL(itemRef)
+  //         .then((url) => {
+  //           this.imageUrl = url;
+  //         })
+  //         .catch((error) => {
+  //           console.error("Erreur lors du chargement de l'image", error);
+  //         });
+  //     }
+  //   })
+  //   .catch((error) => {
+  //     console.error("Erreur lors de la vérification de l'existence de l'objet", error);
+  //   });
+
+  getDownloadURL(imageRef).then(
+    (url) => {
+      // L'image existe, mettez à jour l'URL
+      this.imageUrl = url;
+    }).catch(
+    (error) => {
+      // L'image n'existe pas, gérez les erreurs ici
+      console.error('Erreur lors du chargement de l\'image', error);
+    }
+  );
+
+
 }
 
 // ...
 
-
-  detectFiles(event: any) {
-    this.imageFile = event.target.files[0];
+// comme pour les questionnaires, j'intercepte l'image pour évaluer le poids
+detectFiles(event: any) {
+  const file = event.target.files[0];
+  if (file) {
+    // Vérifie le poids du fichier par rapport au poids maximal autorisé (en octets)
+    const maxSizeInBytes = 10 * 1024 * 1024; // Par exemple, 10 Mo
+    if (file.size <= maxSizeInBytes) {
+      this.imageFile = file;
+      this.imageSelected = true; // Si une image est sélectionnée
+    } else {
+      // Le fichier est trop volumineux, affiche un message d'erreur
+      console.error("Le fichier est trop volumineux. La taille maximale autorisée est de 10 Mo.");
+      this.imageFile = null;
+    }
   }
-
-// ...
+}
 
 // ...
 
@@ -79,7 +103,7 @@ updateImage() {
       'images/trades/' + this.tradeId + '.jpeg'
     );
 
-    // Créez une référence racine
+    // Crée une référence racine
     const rootRef = ref(this.storage);
 
     // Vérifiez d'abord si l'objet existe
@@ -87,14 +111,14 @@ updateImage() {
       .then((result) => {
         const itemRef = result.items.find((item) => item.name === imageRef.name);
         if (itemRef) {
-          // L'ancienne image existe, supprimez-la
+          // L'ancienne image existe, la supprimer
           deleteObject(imageRef)
             .then(() => {
-              // Téléchargez la nouvelle image
+              // Télécharger la nouvelle image
               const blobImage = this.imageFile as Blob;
               uploadBytes(imageRef, blobImage)
                 .then(() => {
-                  // Mettez à jour l'URL de l'image
+                  // Mettre à jour l'URL de l'image
                   getDownloadURL(imageRef)
                     .then((url) => {
                       this.imageUrl = url;
@@ -112,7 +136,7 @@ updateImage() {
               console.error("Erreur lors de la suppression de l'ancienne image", error);
             });
         } else {
-          // L'ancienne image n'existe pas, téléchargez simplement la nouvelle image
+          // L'ancienne image n'existe pas, télécharger simplement la nouvelle image
           const blobImage = this.imageFile as Blob;
           uploadBytes(imageRef, blobImage)
             .then(() => {
@@ -138,9 +162,6 @@ updateImage() {
     console.error("Aucune nouvelle image sélectionnée.");
   }
 }
-
-// ...
-
 
 // ...
 
