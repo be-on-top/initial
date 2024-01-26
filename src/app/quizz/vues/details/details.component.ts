d4915805ffff786e2498a0b20a10805f424b3212
montussan2023! <pwacitypro@gmail.com>
2024-01-26 09:48:31 +0100

    removing handleTouch() for zoom + wording on mac devices install

--- src/app/quizz/vues/details/details.component.ts
+++ src/app/quizz/vues/details/details.component.ts
@@ -167,41 +167,42 @@ export class DetailsComponent implements OnInit {
     private isZoomed = false; 
 
   
-    handleTouch() {
-      const imageElement = this.imageElement;
+    // handleTouch() {
+    //   const imageElement = this.imageElement;
   
-      if (imageElement && imageElement.nativeElement) {
-        const image = imageElement.nativeElement;
+    //   if (imageElement && imageElement.nativeElement) {
+    //     const image = imageElement.nativeElement;
   
-        // Inverse l'état de zoom
-        this.isZoomed = !this.isZoomed;
+    //     // Inverse l'état de zoom
+    //     this.isZoomed = !this.isZoomed;
   
-        // Ajoute ou retire la classe "zoomed" selon l'état actuel
-        if (this.isZoomed) {
-          this.renderer.addClass(image, 'zoomed');
-        } else {
-          this.renderer.removeClass(image, 'zoomed');
-        }
-      }
-    }
+    //     // Ajoute ou retire la classe "zoomed" selon l'état actuel
+    //     if (this.isZoomed) {
+    //       this.renderer.addClass(image, 'zoomed');
+    //     } else {
+    //       this.renderer.removeClass(image, 'zoomed');
+    //     }
+    //   }
+    // }
   
-    @HostListener('document:click', ['$event'])
-    handleDocumentClick(event: Event) {
-      const imageElement = this.imageElement;
+    // @HostListener('document:click', ['$event'])
+    // handleDocumentClick(event: Event) {
+    //   const imageElement = this.imageElement;
   
-      if (imageElement && imageElement.nativeElement) {
-        const image = imageElement.nativeElement;
+    //   if (imageElement && imageElement.nativeElement) {
+    //     const image = imageElement.nativeElement;
   
-        // Vérifie si l'élément cliqué est l'élément de l'image
-        const isClickOnImage = image.contains(event.target as Node);
+    //     // Vérifie si l'élément cliqué est l'élément de l'image
+    //     const isClickOnImage = image.contains(event.target as Node);
   
-        // Si le clic n'est pas sur l'image et l'image est zoomée, réinitialise
-        if (!isClickOnImage && this.isZoomed) {
-          this.isZoomed = false;
-          this.renderer.removeClass(image, 'zoomed');
-        }
-      }
-    }
+    //     // Si le clic n'est pas sur l'image et l'image est zoomée, réinitialise
+    //     if (!isClickOnImage && this.isZoomed) {
+    //       this.isZoomed = false;
+    //       this.renderer.removeClass(image, 'zoomed');
+    //     }
+    //   }
+    // }
+
   }
 
   
