3e91e8da50f9142f7ec3c0a68020fdfc519a3bba
montussan2023! <pwacitypro@gmail.com>
2024-03-08 12:43:53 +0100

    role-guard with roles collection to be continued

--- src/app/role-guard.guard.ts
+++ src/app/role-guard.guard.ts
@@ -3,6 +3,8 @@ import { Auth, onAuthStateChanged } from '@angular/fire/auth';
 import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot, UrlTree } from '@angular/router';
 import { Observable } from 'rxjs';
 import { EvaluatorsService } from './admin/evaluators.service';
+import { Firestore, doc, docData } from '@angular/fire/firestore';
+Firestore
 
 @Injectable({
   providedIn: 'root'
@@ -11,34 +13,48 @@ export class RoleGuardGuard implements CanActivate {
 
   // https://www.youtube.com/watch?v=g3pq8WamEmE&t=3s
 
-  constructor(private auth:Auth, private evaluatorService:EvaluatorsService){}
+  constructor(private auth: Auth, private evaluatorService: EvaluatorsService, private firestore: Firestore) { }
 
   canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): any {
     // return true;
     return this.isAuthorized(route)
   }
 
-  private isAuthorized(route:any){
+  private isAuthorized(route: any) {
 
-    const roles:any=[]
+    // const roles: any = []
     onAuthStateChanged(this.auth, (user: any) => {
       if (user) {
-      
-        console.log("log user uid depuis le header", user.uid);   
-        this.evaluatorService.getEvaluator(user.uid).subscribe(data=>{
-          // let isAuthorized=false
-          // console.log("data de getEvaluator depuis Role Guard Service", data);
-          // console.log("roles depuis Role Guard Service", data.roles);
-          roles.push(data.roles)
-          console.log(roles, "tableau des roles utilisateurs depuis Role Guard");
+
+        console.log("log user uid depuis role guard", user.uid);
+        // à finir...
+        // this.getRole(user.uid).subscribe(data => {
+        //   console.log("data de l'utilisateur depuis header", data);
+        //   console.log("roles depuis role guard", data.role);
+
+
+        //   const expectedRoles = route.data['expectedRoles']
+        //   console.log(expectedRoles);
           
-          const expectedRoles=route.data['expectedRoles']
-          const rolesMatches = roles.findIndex((role:any)=>expectedRoles.indexOf(role))
-          rolesMatches<0||rolesMatches?false:true
-      })}
+        //   const rolesMatches = data.role.findIndex((role: any) => expectedRoles.indexOf(role))
+        //   rolesMatches < 0 || rolesMatches ? false : true
+
+        //   console.log(rolesMatches);
+          
+
+        // })
+
+      }
     })
 
+  }
+
+  getRole(id: any) {
+    // finalement, compte tenu du fait que les evaluators peuvent potentiellement aussi être des tuteurs (formateurs) roles sera un tableau
+    // au niveau de getRole, cela ne change pas grand chose
+    let $roleRef = doc(this.firestore, "roles/" + id)
+    return docData($roleRef) as Observable<any>;
 
   }
-  
+
 }
