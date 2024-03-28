dcd9c981d80837a04d40f4a609500497c4d1a0d5
montussan2023! <pwacitypro@gmail.com>
2024-03-28 10:05:30 +0100

    fixing methods for displaying && closing banner

--- src/app/cookie-consent-banner/cookie-consent-banner.component.ts
+++ src/app/cookie-consent-banner/cookie-consent-banner.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, DoCheck, OnInit } from '@angular/core';
 import { ConsentService } from '../consent.service';
 
 @Component({
@@ -6,18 +6,39 @@ import { ConsentService } from '../consent.service';
   templateUrl: './cookie-consent-banner.component.html',
   styleUrls: ['./cookie-consent-banner.component.css']
 })
-export class CookieConsentBannerComponent {
+export class CookieConsentBannerComponent implements OnInit {
 
-  constructor(public consentService: ConsentService) {}
+  showBanner = true; // Affiche la bannière par défaut
+
+  constructor(public consentService: ConsentService) {
+
+    // Vérifier si l'utilisateur a déjà pris une décision concernant les cookies
+    if (this.consentService.getConsent()) {
+      this.showBanner = false; // Masquer la bannière si l'utilisateur a déjà donné son consentement
+    }
+
+  }
+
+  ngOnInit(): void {
+    // Vérifier si l'utilisateur a déjà pris une décision concernant les cookies
+    if (this.consentService.getConsent()) {
+      this.showBanner = false; // Masquer la bannière si l'utilisateur a déjà donné son consentement
+    }
+  }
 
   acceptCookies() {
     this.consentService.setConsent(true);
     // Autres actions nécessaires après avoir accepté les cookies
+    this.showBanner = false;
   }
 
   rejectCookies() {
     this.consentService.setConsent(false);
+    console.log('Bannière masquée après refus des cookies');
     // Autres actions nécessaires après avoir refusé les cookies
+    this.showBanner = false;
   }
+
+
 
 }
