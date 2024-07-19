ae9ece58df079f58a4c0e95d1a2534674f746f4c
montussan2023! <pwacitypro@gmail.com>
2024-07-19 16:06:42 +0200

    minor fixings on benefits for addingTag testing & integrated h3 elements on text

--- src/app/benefits/benefits.component.ts
+++ src/app/benefits/benefits.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Meta } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-benefits',
   templateUrl: './benefits.component.html',
   styleUrls: ['./benefits.component.css']
 })
-export class BenefitsComponent {
+export class BenefitsComponent implements OnInit {
+
+  constructor(private metaService: Meta) {
+
+  }
+
+  ngOnInit(): void {
+    this.addTag()
+  }
+
+
+  addTag() {
+    this.metaService.updateTag({ name: 'description', content: 'Les questionnaires d\'évaluation en ligne, dits d\'évaluation initiale sont conçus par des experts métiers, professionnels de terrain, en connaissance des compétences professionnelles, savoir-faire professionnels, savoir-être professionnels, aptitudes et qualités requises par les recruteurs et entreprises' })
+    this.metaService.addTag({ name: 'robots', content: 'index,follow' })
+    this.metaService.updateTag({ property: 'og:title', content: 'Informations Utilisateurs : Formations et compétences professionnelles évaluées sur BE-ON-top.io par des professionnels de la formation' })
+    this.metaService.updateTag({ property: 'og:description', content: 'Les questionnaires d\'évaluation en ligne, dits d\'évaluation initiale sont conçus par des experts métiers, professionnels de terrain, en connaissance des compétences professionnelles, savoir-faire professionnels, savoir-être professionnels, aptitudes et qualités requises par les recruteurs et entreprises' })
+  }
 
 }
