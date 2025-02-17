1af1fa2aefe1ef9ba582b284e7ff56c2aceb44f6
montussan2023! <pwacitypro@gmail.com>
2025-02-17 16:27:41 +0100

    test footer if userConsent

--- src/app/footer/footer/footer.component.ts
+++ src/app/footer/footer/footer.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from 'src/app/admin/auth.service';
 
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.css']
 })
-export class FooterComponent {
+export class FooterComponent implements OnInit {
+  showFooter = false; 
 
+  constructor(
+    // private authService: AuthService
+  ) {}
+
+  ngOnInit() {
+    this.checkFooterVisibility();
+  }
+
+  private checkFooterVisibility() {
+     // Vérifier si l'utilisateur est authentifié
+
+    const hasUserConsent = localStorage.getItem('userConsent') !== null; // Vérifier le consentement
+
+    this.showFooter = hasUserConsent; // Afficher le footer si les 2 conditions sont remplies
+  }
 }
