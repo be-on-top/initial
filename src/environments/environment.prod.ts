7f3597cd37b54032cf9610f1939b14f10f160208
montussan2023! <pwacitypro@gmail.com>
2025-03-28 11:03:05 +0100

    addionnal info on contextual message for form not sent + discard testing environment.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,12 +1,12 @@
 export const environment = {
   production: true,
   firebaseConfig: {
-    apiKey: "AIzaSyB1X61jdRVtw_1GMVJYHKFLUFXqujg15-c",
-    authDomain: "ng-users-93f69.firebaseapp.com",
-    projectId: "ng-users-93f69",
-    storageBucket: "ng-users-93f69.appspot.com",
-    messagingSenderId: "350108679764",
-    appId: "1:350108679764:web:357e5a8a324da7ad0777c1"
+    // apiKey: "AIzaSyB1X61jdRVtw_1GMVJYHKFLUFXqujg15-c",
+    // authDomain: "ng-users-93f69.firebaseapp.com",
+    // projectId: "ng-users-93f69",
+    // storageBucket: "ng-users-93f69.appspot.com",
+    // messagingSenderId: "350108679764",
+    // appId: "1:350108679764:web:357e5a8a324da7ad0777c1"
 
     // base vierge
     // apiKey: "AIzaSyCSumVFe5c414G6t0NlSp6LIPfXgTs5IEk",
@@ -18,12 +18,12 @@ export const environment = {
     // measurementId: "G-4D90TV8L6W"
 
     // application officielle
-    // apiKey: "AIzaSyCSumVFe5c414G6t0NlSp6LIPfXgTs5IEk",
-    // authDomain: "be-on-top-beta.firebaseapp.com",
-    // projectId: "be-on-top-beta",
-    // storageBucket: "be-on-top-beta.appspot.com",
-    // messagingSenderId: "212539373061",
-    // appId: "1:212539373061:web:425cde1b1071152e700e6c",
-    // measurementId: "G-C9M2VS675H"
+    apiKey: "AIzaSyCSumVFe5c414G6t0NlSp6LIPfXgTs5IEk",
+    authDomain: "be-on-top-beta.firebaseapp.com",
+    projectId: "be-on-top-beta",
+    storageBucket: "be-on-top-beta.appspot.com",
+    messagingSenderId: "212539373061",
+    appId: "1:212539373061:web:425cde1b1071152e700e6c",
+    measurementId: "G-C9M2VS675H"
   }
 };
