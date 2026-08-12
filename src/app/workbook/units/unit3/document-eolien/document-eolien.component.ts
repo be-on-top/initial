a2acf70b26c07e8cbf7b380156bc3096f5270e01
montussan2023! <pwacitypro@gmail.com>
2026-08-12 12:21:35 +0200

    feat(unit3): add QCM scoring rules and shared document for exercises 11-17

---
+++ src/app/workbook/units/unit3/document-eolien/document-eolien.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-document-eolien',
+  templateUrl: './document-eolien.component.html',
+  styleUrls: ['./document-eolien.component.css']
+})
+export class DocumentEolienComponent {
+
+}
